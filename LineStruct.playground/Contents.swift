b5aaec985dd84f352f5c1ad9024f53b7dd6b9c73
aabato <angelica.bato@gmail.com>
2016-07-20 19:41:39 -0400

    Clean up lab

--- LineStruct.playground/Contents.swift
+++ LineStruct.playground/Contents.swift
@@ -13,14 +13,10 @@ struct Point {
 
 
 
-struct Line {
-    var endPoint1: Point = Point(x: 0.0, y: 0.0)
-    var endPoint2: Point = Point(x: 0.0, y: 0.0)
-    
-    func length()->Double {
-        return sqrt(pow((endPoint2.y - endPoint1.y), 2.0) + pow((endPoint2.x - endPoint1.x), 2.0))
-    }
-}
+
+
+
+
 
 //var point1 = Point(x: 5.0, y: 0.0)
 //var point2 = Point(x: 2.0, y: 4.0)
